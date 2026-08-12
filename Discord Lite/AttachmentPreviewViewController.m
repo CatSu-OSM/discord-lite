d55df27f95c817b0dad42cec90c4fd9e0e6d2025
CatSu-OSM <117058088+CatSu-OSM@users.noreply.github.com>
2026-08-11 22:58:40 -0500

    Replace attachment preview and preferences XIBs

--- Discord Lite/AttachmentPreviewViewController.m
+++ Discord Lite/AttachmentPreviewViewController.m
@@ -8,7 +8,66 @@
 
 #import "AttachmentPreviewViewController.h"
 
+static NSTextField *DLAttachmentPreviewLabel(NSString *title, NSRect frame, NSFont *font) {
+    NSTextField *label = [[NSTextField alloc] initWithFrame:frame];
+    [label setStringValue:title];
+    [label setEditable:NO];
+    [label setBezeled:NO];
+    [label setDrawsBackground:NO];
+    [label setSelectable:NO];
+    [label setFont:font];
+    return [label autorelease];
+}
+
 @implementation AttachmentPreviewViewController
+
+-(id)init {
+    self = [super init];
+    if (self) {
+        nonImageAttachmentView = [[NSView_BGColor alloc] initWithFrame:NSMakeRect(0.0f, 0.0f, 275.0f, 62.0f)];
+
+        NSBox *box = [[NSBox alloc] initWithFrame:NSMakeRect(2.0f, 1.0f, 271.0f, 58.0f)];
+        [box setTitlePosition:NSNoTitle];
+        [box setBorderType:NSLineBorder];
+        [box setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+        [nonImageAttachmentView addSubview:box];
+
+        NSView *boxView = [box contentView];
+        fileIconImageView = [[NSImageView alloc] initWithFrame:NSMakeRect(7.0f, 10.0f, 43.0f, 38.0f)];
+        [fileIconImageView setImageScaling:NSImageScaleProportionallyDown];
+        [boxView addSubview:fileIconImageView];
+
+        filenameTextField = [[NSTextField alloc] initWithFrame:NSMakeRect(56.0f, 33.0f, 197.0f, 17.0f)];
+        [filenameTextField setEditable:NO];
+        [filenameTextField setBezeled:NO];
+        [filenameTextField setDrawsBackground:NO];
+        [[filenameTextField cell] setLineBreakMode:NSLineBreakByTruncatingTail];
+        [boxView addSubview:filenameTextField];
+
+        sizeTextField = [DLAttachmentPreviewLabel(@"Size", NSMakeRect(56.0f, 17.0f, 107.0f, 14.0f), [NSFont smallSystemFontSize]] retain];
+        [boxView addSubview:sizeTextField];
+
+        downloadButton = [[NSButton alloc] initWithFrame:NSMakeRect(164.0f, 2.0f, 103.0f, 32.0f)];
+        [downloadButton setTitle:@"Download"];
+        [downloadButton setBezelStyle:NSRoundedBezelStyle];
+        [downloadButton setTarget:self];
+        [downloadButton setAction:@selector(downloadAttachment:)];
+        [downloadButton setAutoresizingMask:NSViewMinXMargin | NSViewMinYMargin];
+        [boxView addSubview:downloadButton];
+
+        downloadProgressIndicator = [[NSProgressIndicator alloc] initWithFrame:NSMakeRect(170.0f, 9.0f, 91.0f, 12.0f)];
+        [downloadProgressIndicator setIndeterminate:YES];
+        [downloadProgressIndicator setHidden:YES];
+        [downloadProgressIndicator setAutoresizingMask:NSViewWidthSizable | NSViewMinYMargin];
+        [boxView addSubview:downloadProgressIndicator];
+        [box release];
+    }
+    return self;
+}
+
+-(id)initWithNibNamed:(NSString *)inNibName bundle:(NSBundle *)bundle {
+    return [self init];
+}
 
 -(NSView *)attachmentView {
     return attachmentView;
@@ -75,7 +134,15 @@
 -(void)dealloc {
     [eventHandlerView setDelegate:nil];
     [eventHandlerView release];
-    [attachmentView release];
+    if (attachmentView != nonImageAttachmentView) {
+        [attachmentView release];
+    }
+    [nonImageAttachmentView release];
+    [fileIconImageView release];
+    [filenameTextField release];
+    [sizeTextField release];
+    [downloadProgressIndicator release];
+    [downloadButton release];
     [representedObject setPreviewDelegate:nil];
     [representedObject release];
     [attachmentViewerWindow setDelegate:nil];
