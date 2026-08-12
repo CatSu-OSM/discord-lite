9cacffb5d83b73dd6bfcddef45aec9af45bd2652
CatSu-OSM <117058088+CatSu-OSM@users.noreply.github.com>
2026-08-11 23:59:49 -0500

    Restore borderless server icon rendering

--- Discord Lite/ServerItemViewController.m
+++ Discord Lite/ServerItemViewController.m
@@ -24,7 +24,6 @@ const CGFloat SELECTED_AVATAR_RADIUS = 16.5f;
         selectionButton = [[NSButton alloc] initWithFrame:NSMakeRect(12, 3, 54, 54)];
         [selectionButton setBezelStyle:NSShadowlessSquareBezelStyle];
         [selectionButton setBordered:NO];
-        [selectionButton setTransparent:YES];
         [selectionButton setImagePosition:NSImageOnly];
         [[selectionButton cell] setHighlightsBy:NSNoCellMask];
         [[selectionButton cell] setShowsStateBy:NSNoCellMask];
